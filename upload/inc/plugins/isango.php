f21c70c50946890d11eb3075e7771e3220297c29
effone <effone@mybb.com>
2019-08-11 23:57:54 +0530

    UCP Connection Module

--- upload/inc/plugins/isango.php
+++ upload/inc/plugins/isango.php
@@ -6,6 +6,8 @@ if (!defined("IN_MYBB")) {
 
 $plugins->add_hook('global_start', 'isango_buttons');
 $plugins->add_hook('member_login', 'isango_bridge');
+$plugins->add_hook('usercp_menu', 'isango_ucpnav', 25);
+$plugins->add_hook('usercp_start', 'isango_connections');
 $plugins->add_hook('admin_settings_print_peekers', 'isango_settingspeekers');
 
 function isango_info()
@@ -27,34 +29,33 @@ function isango_activate()
     global $db, $lang;
     $lang->load('isango');
 
-	$stylesheet = @file_get_contents(MYBB_ROOT.'inc/plugins/isango/isango.css');
-	$attachedto = '';
-	$name = 'isango.css';
-	$css = array(
-		'name' => $name,
-		'tid' => 1,
-		'attachedto' => $db->escape_string($attachedto),
-		'stylesheet' => $db->escape_string($stylesheet),
-		'cachefile' => $name,
-		'lastmodified' => TIME_NOW,
-	);
-	$db->update_query('themestylesheets', array(
-		"attachedto" => $attachedto
-	), "name='{$name}'");
-	$query = $db->simple_select('themestylesheets', 'sid', "tid='1' AND name='{$name}'");
-	$sid = (int) $db->fetch_field($query, 'sid');
-	if ($sid) {
-		$db->update_query('themestylesheets', $css, "sid='{$sid}'");
-	} else {
-		$sid = $db->insert_query('themestylesheets', $css);
-		$css['sid'] = (int) $sid;
-	}
-	require_once MYBB_ADMIN_DIR."inc/functions_themes.php";
-	if (!cache_stylesheet(1, $css['cachefile'], $stylesheet))
-	{
-		$db->update_query("themestylesheets", array('cachefile' => "css.php?stylesheet={$sid}"), "sid='{$sid}'", 1);
-	}
-    update_theme_stylesheet_list(1, false, true);    
+    $stylesheet = @file_get_contents(MYBB_ROOT . 'inc/plugins/isango/isango.css');
+    $attachedto = '';
+    $name = 'isango.css';
+    $css = array(
+        'name' => $name,
+        'tid' => 1,
+        'attachedto' => $db->escape_string($attachedto),
+        'stylesheet' => $db->escape_string($stylesheet),
+        'cachefile' => $name,
+        'lastmodified' => TIME_NOW,
+    );
+    $db->update_query('themestylesheets', array(
+        "attachedto" => $attachedto,
+    ), "name='{$name}'");
+    $query = $db->simple_select('themestylesheets', 'sid', "tid='1' AND name='{$name}'");
+    $sid = (int) $db->fetch_field($query, 'sid');
+    if ($sid) {
+        $db->update_query('themestylesheets', $css, "sid='{$sid}'");
+    } else {
+        $sid = $db->insert_query('themestylesheets', $css);
+        $css['sid'] = (int) $sid;
+    }
+    require_once MYBB_ADMIN_DIR . "inc/functions_themes.php";
+    if (!cache_stylesheet(1, $css['cachefile'], $stylesheet)) {
+        $db->update_query("themestylesheets", array('cachefile' => "css.php?stylesheet={$sid}"), "sid='{$sid}'", 1);
+    }
+    update_theme_stylesheet_list(1, false, true);
 
     $gid = (int) ($db->fetch_field($db->simple_select("settinggroups", "gid", "name='isango'"), "gid"));
     $isango_opts = array();
@@ -107,7 +108,7 @@ function isango_activate()
     rebuild_settings();
 
     require MYBB_ROOT . "inc/adminfunctions_templates.php";
-    foreach(['header_welcomeblock_guest','member_login','member_register'] as $tpl){
+    foreach (['header_welcomeblock_guest', 'member_login', 'member_register'] as $tpl) {
         find_replace_templatesets($tpl, '#<\/form>#', '</form><!-- isango -->{$isango_buttons}<!-- /isango -->');
     }
 }
@@ -115,15 +116,18 @@ function isango_activate()
 function isango_deactivate()
 {
     global $db;
-    foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator(MYBB_ROOT.'cache/themes')) as $file){
-        if(stripos($file , 'isango') !== false) @unlink($file);
+    foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(MYBB_ROOT . 'cache/themes')) as $file) {
+        if (stripos($file, 'isango') !== false) {
+            @unlink($file);
+        }
+
     }
-	$db->delete_query('themestylesheets', "name='isango.css'");
-	require_once MYBB_ADMIN_DIR."inc/functions_themes.php";
+    $db->delete_query('themestylesheets', "name='isango.css'");
+    require_once MYBB_ADMIN_DIR . "inc/functions_themes.php";
     update_theme_stylesheet_list(1, false, true);
-    
+
     require MYBB_ROOT . "inc/adminfunctions_templates.php";
-    foreach(['header_welcomeblock_guest','member_login','member_register'] as $tpl){
+    foreach (['header_welcomeblock_guest', 'member_login', 'member_register'] as $tpl) {
         find_replace_templatesets($tpl, '#\<!--\sisango\s--\>(.+)\<!--\s\/isango\s--\>#is', '', 0);
     }
 }
@@ -132,6 +136,30 @@ function isango_install()
 {
     global $db, $lang;
     $lang->load('isango');
+
+    // Install Isango templates
+    foreach (glob(MYBB_ROOT . 'inc/plugins/isango/*.htm') as $template) {
+        $db->insert_query('templates', array(
+            'title' => $db->escape_string(strtolower(basename($template, '.htm'))),
+            'template' => $db->escape_string(@file_get_contents($template)),
+            'sid' => -2,
+            'version' => 100,
+            'dateline' => TIME_NOW
+        ));
+    }
+
+    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "isango (
+		cid int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+		uid int(10) UNSIGNED NOT NULL,
+		gateway varchar(30) NOT NULL DEFAULT 'unknown',
+		cuid varchar(160),
+		email varchar(220) NOT NULL DEFAULT '',
+		name varchar(220) NOT NULL DEFAULT '',
+		dateline int(10) UNSIGNED NOT NULL,
+		PRIMARY KEY (`cid`),
+		INDEX (`cuid`, `uid`)
+		);"
+    );
 
     $isango_group = array(
         'name' => 'isango',
@@ -157,6 +185,11 @@ function isango_is_installed()
 function isango_uninstall()
 {
     global $db;
+    // $db->query("drop TABLE ".TABLE_PREFIX."isango");
+
+    foreach (glob(MYBB_ROOT . 'inc/plugins/isango/*.htm') as $template) {
+        $db->delete_query('templates', 'title = "' . strtolower(basename($template, '.htm')) . '"');
+    }
 
     $db->delete_query("settings", "name LIKE '%isango_%'");
     $db->delete_query("settinggroups", "name='isango'");
@@ -188,17 +221,20 @@ function isango_bridge()
                     // Get the access token
                     $params = array('code' => $mybb->input['code'], 'state' => $mybb->input['state']);
                     $data = isango_curl($params, $mybb->input['gateway'], 'token');
-                    
+
                     // Get user information
                     $user = array();
                     $conf = isango_config($mybb->input['gateway'], 'api');
-                    if(is_string($conf['url'])) $conf['url'] = (array)$conf['url'];
+                    if (is_string($conf['url'])) {
+                        $conf['url'] = (array) $conf['url'];
+                    }
+
                     foreach ($conf['url'] as $url) {
                         $params = array('code' => $data['access_token'], 'url' => $url);
                         $response = isango_curl($params, $mybb->input['gateway']);
                         $user = array_merge($user, $response);
                     }
-                    
+
                 } catch (Exception $e) {
                     $errors = $e->getMessage();
                 }
@@ -225,7 +261,7 @@ function isango_bridge()
                     'client_id' => $mybb->settings['isango_' . $gateway . '_id'],
                     'redirect_uri' => $mybb->settings['bburl'] . '/member.php?action=login&gateway=' . $gateway,
                     'state' => $state,
-                    'response_type' => 'code'
+                    'response_type' => 'code',
                 ), $conf['params']);
 
                 // Redirect the user to authorization page
@@ -238,67 +274,110 @@ function isango_bridge()
 
 function isango_login($user, $gateway)
 {
-    global $db, $lang;
+    global $db, $lang, $mybb;
     $lang->load('isango');
     extract(isango_fetchinfo($user, $gateway));
-    
+
+    // Check for banned email
+    if (is_banned_email($email, true)) {
+        return $lang->auth_email_banned;
+    }
+
     // Check availability of username by email
-    $query = $db->simple_select("users", "uid, loginkey", "email='{$email}'");
+    $query = $db->query("
+        SELECT u.uid, u.loginkey
+        FROM " . TABLE_PREFIX . "users u
+        LEFT JOIN " . TABLE_PREFIX . "isango i ON (i.uid=u.uid)
+        WHERE u.email='{$email}'
+        OR (i.gateway='{$gateway}' AND i.email='{$email}')
+        OR (i.gateway='{$gateway}' AND i.cuid='{$id}')
+    ");
     $user_info = $db->fetch_array($query);
 
-    if (!$user_info) { // User not found, need to register a fresh account
-        // Check for banned email
-        if (is_banned_email($email, true)) {
-            return $lang->auth_email_banned;
-        }
-        // Accumulate all possible usernames based on available data
-        $possible_usernames = array();
-        $temp1 = explode('@', $email);
-        $possible_usernames[] = isango_purename($temp1[0]);
-        $temp2 = explode(' ', $name);
-        $possible_usernames[] = isango_purename($temp2[0]);
-        $possible_usernames[] = isango_purename($temp1[0] . '@' . $gateway);
-        if (count($temp2) > 1) {
-            $possible_usernames[] = isango_purename(implode('_', $temp2));
-        }
-        $possible_usernames = array_values(array_filter($possible_usernames));
-
-        $i = 0;
-        do {
-            $username = $possible_usernames[$i++];
-            if (get_user_by_username(trim($username), ['exists' => true])) {
-                $username = '';
-            } else {
-                $i = count($possible_usernames);
-            }
-        } while ($i < count($possible_usernames));
-
-        if (empty($username)) {
-            // Loop over, we don't have a username to use, OMG!
-            return $lang->undetermined_username; // Improve the situation!!!!!!!
-        } else {
-            // Walla!!! got a name. Use it for new user registration
-            require_once MYBB_ROOT . "inc/datahandlers/user.php";
-            $userhandler = new UserDataHandler("insert");
-            // Set the data for the new user.
-            $userhandler->set_data(array(
-                "username" => $username,
-                "password" => base64_encode(random_bytes(10)).'aZ9', // Randpm pass, PHP 7+
-                "email" => $email,
-                "email2" => $email,
-                "usergroup" => 2,
-                "regip" => $session->packedip,
-                "registration" => true,
-            ));
-            if (!$userhandler->validate_user()) {
-                return $userhandler->get_friendly_errors();
-            }
-        }
-        $user_info = $userhandler->insert_user();
+    // We need to reconfirm about the gateway entry
+    $connected = 0;
+    $logged_in = $mybb->user['uid'];
+    if ($user_info) {
+        $connected = $db->num_rows($db->simple_select('isango', 'uid', "(gateway='{$gateway}' AND email='{$email}') OR (gateway='{$gateway}' AND cuid='{$id}')"));
     }
-    // We have the user with us, let's log the user in
-    my_setcookie("mybbuser", $user_info['uid'] . "_" . $user_info['loginkey'], null, true, "lax");
-    redirect("index.php", $lang->sprintf($lang->auth_success_redirect, ucfirst($gateway)));
+
+    if (!$logged_in) {
+        if(!$user_info){ // User not found, need to register a fresh account
+            // Accumulate all possible usernames based on available data
+            $possible_usernames = array();
+            $temp1 = explode('@', $email);
+            $possible_usernames[] = isango_purename($temp1[0]);
+            $temp2 = explode(' ', $name);
+            $possible_usernames[] = isango_purename($temp2[0]);
+            $possible_usernames[] = isango_purename($temp1[0] . '@' . $gateway);
+            if (count($temp2) > 1) {
+                $possible_usernames[] = isango_purename(implode('_', $temp2));
+            }
+            $possible_usernames = array_values(array_filter($possible_usernames));
+
+            $i = 0;
+            do {
+                $username = $possible_usernames[$i++];
+                if (get_user_by_username(trim($username), ['exists' => true])) {
+                    $username = '';
+                } else {
+                    $i = count($possible_usernames);
+                }
+            } while ($i < count($possible_usernames));
+
+            if (empty($username)) {
+                // Loop over, we don't have a username to use, OMG!
+                return $lang->undetermined_username; // Improve the situation!!!!!!!
+            } else {
+                // Walla!!! got a name. Use it for new user registration
+                require_once MYBB_ROOT . "inc/datahandlers/user.php";
+                $userhandler = new UserDataHandler("insert");
+                // Set the data for the new user.
+                $userhandler->set_data(array(
+                    "username" => $username,
+                    "password" => base64_encode(random_bytes(10)) . 'aZ9', // Randpm pass, PHP 7+
+                    "email" => $email,
+                    "email2" => $email,
+                    "usergroup" => 2,
+                    "regip" => $session->packedip,
+                    "registration" => true,
+                ));
+                if (!$userhandler->validate_user()) {
+                    return $userhandler->get_friendly_errors();
+                }
+            }
+            $user_info = $userhandler->insert_user();
+        }
+
+        $redirect_url = 'index.php';
+        $redirect_message = $lang->sprintf($lang->auth_success_registered_redirect, ucfirst($gateway));
+
+        // We have the user with us, let's log the user in
+        my_setcookie("mybbuser", $user_info['uid'] . "_" . $user_info['loginkey'], null, true, "lax");
+    } else { // User already logged in
+        $user_info['uid'] = $mybb->user['uid'];
+        $redirect_url = 'usercp.php?action=connections';
+        if ($connected) {
+            $redirect_message = $lang->sprintf($lang->auth_already_connected_redirect, ucfirst($gateway));
+        } else {
+            $redirect_message = $lang->sprintf($lang->auth_success_connected_redirect, ucfirst($gateway));
+        }
+    }
+
+    // Make the connection entry
+    if (!$connected) {
+        $connected = array(
+            'uid' => $user_info['uid'],
+            'gateway' => $gateway,
+            'cuid' => $id,
+            'email' => $email,
+            'name' => $name,
+            'dateline' => TIME_NOW,
+        );
+        $db->insert_query("isango", $connected);
+    }
+
+    redirect($redirect_url, $redirect_message);
 }
 
 function isango_purename($username)
@@ -323,15 +402,14 @@ function isango_purename($username)
 
 function isango_fetchinfo($u, $gateway)
 {
-	global $db;
-	$info = array();
+    global $db;
+    $info = array();
     $conf = isango_config($gateway, 'info');
-	foreach($conf as $key => $val)
-	{
-		eval("\$val = \"".$val."\";");
-		$info[$key] = $db->escape_string($val);
-	}
-	return $info;
+    foreach ($conf as $key => $val) {
+        eval("\$val = \"" . $val . "\";");
+        $info[$key] = $db->escape_string($val);
+    }
+    return $info;
 }
 
 function isango_curl(array $params, string $gateway, string $mode = 'api')
@@ -385,7 +463,7 @@ function isango_curl(array $params, string $gateway, string $mode = 'api')
     return $data;
 }
 
-function isango_buttons()
+function isango_buttons($return = false)
 {
     global $isango_buttons;
     $isango_buttons = "";
@@ -394,9 +472,10 @@ function isango_buttons()
             $isango_buttons .= '<a class="button isango_' . $gateway . '" href="member.php?action=login&gateway=' . $gateway . '"><span>' . ucfirst($gateway) . '</span></a>';
         }
     }
-    if(!empty($isango_buttons)){
+    if (!empty($isango_buttons)) {
         $isango_buttons = "<div style='text-align: center; margin-top: 10px;'>" . $isango_buttons . "</div>";
     }
+    if($return) return $isango_buttons;
 }
 
 function isango_gateway_error(string $gateway)
@@ -448,4 +527,52 @@ function isango_config(string $gateway = "", string $mode = "")
         return $conf;
     }
     return false;
+}
+
+function isango_ucpnav()
+{
+    global $usercpmenu;
+    $usercpmenu = substr_replace($usercpmenu, '<tr><td class="trow1 smalltext"><a href="usercp.php?action=connections" class="usercp_nav_item usercp_nav_connections">Connections</a></td></tr></tbody>', strrpos($usercpmenu, '</tbody>'), strlen('</tbody>'));
+}
+
+function isango_connections()
+{
+    global $mybb, $lang, $header, $footer, $headerinclude, $templates, $theme, $usercpnav, $db;
+    $lang->load('isango');
+    add_breadcrumb($lang->nav_usercp, "usercp.php");
+
+    if ($mybb->input['action'] == "delete_connections" && $mybb->request_method == "post")
+    {
+        verify_post_check($mybb->get_input('my_post_key'));
+
+        if($_POST['cid']){
+            $cids = implode(',', array_map('intval', $_POST['cid']));
+            $db->delete_query("isango", 'uid="'.$mybb->user['uid'].'" AND cid IN ('.$cids.')');
+        }
+        
+        $mybb->input['action'] = "connections";
+    }
+
+    if ($mybb->input['action'] == "connections")
+    {
+        add_breadcrumb($lang->isango_nav_connections);
+        $connections = '';
+        $query = $db->simple_select('isango', '*', 'uid="' . $mybb->user['uid'] . '"');
+        while ($conn = $db->fetch_array($query)) {
+            $alt_row = alt_trow();
+            $conn['gateway'] = ucfirst($conn['gateway']);
+            $conn['dateline'] = my_date($mybb->settings['dateformat'], $conn['dateline']);
+            eval("\$connections .= \"" . $templates->get("usercp_connections_connection") . "\";");
+        }
+
+        if (empty($connections)) {
+            eval("\$connections = \"" . $templates->get("usercp_connections_none") . "\";");
+        }
+
+        $isango_buttons = isango_buttons(true);
+        if(empty($isango_buttons)) $isango_buttons = $lang->isango_no_service;
+
+        eval("\$connect_page = \"" . $templates->get("usercp_connections") . "\";");
+        output_page($connect_page);
+    }
 }
